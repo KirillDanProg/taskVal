57df332187c7370b89f5b34f4e747d2da4bbaac7
KirillDanProg <kirill2898v@gmail.com>
2024-02-27 18:33:51 +0300

    feat: mutation hooks removed, data returns only when get_items action is finished

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,29 +1,30 @@
 import { useEffect } from "react";
-import {
-  useGetProductsIdsMutation,
-  useGetProductsMutation,
-} from "@/services/productsApi";
+import { useGetEntitiesByActionMutation } from "@/services/productsApi";
 
-export const useGetProducts = params => {
-  const [getProductsIds, { error: productsIdsError, isLoading: idsLoading }] =
-    useGetProductsIdsMutation();
-  const [
-    getProductsByIds,
-    { data: products, isLoading: productsLoading, error: productsError },
-  ] = useGetProductsMutation();
-
-  const getProductsByIdsHandler = async () => {
-    const { data: productIds } = await getProductsIds(params);
-    if (productIds) {
-      await getProductsByIds({ ids: productIds });
-    }
-  };
-
-  const error = productsIdsError || productsError;
+export const useGetProducts = offset => {
+  const [getEntities, { data, error, isLoading, originalArgs }] = useGetEntitiesByActionMutation();
+  const isProducts = originalArgs?.action === "get_items";
 
   useEffect(() => {
+    const getProductsByIdsHandler = async () => {
+      const { data: productIds } = await getEntities({
+        action: "get_ids",
+        params: {
+          offset,
+          limit: 50,
+        },
+      });
+      if (productIds) {
+        await getEntities({
+          action: "get_items",
+          params: {
+            ids: productIds,
+          },
+        });
+      }
+    };
     getProductsByIdsHandler();
-  }, [error, params]);
+  }, [offset, error]);
 
-  return { products, idsLoading, productsLoading, error };
+  return { data: isProducts ? data : undefined, isLoading, error };
 };
