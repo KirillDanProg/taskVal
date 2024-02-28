57f0b9989f3dbc6a236839b5eccf92be689edc75
KirillDanProg <kirill2898v@gmail.com>
2024-02-28 20:33:34 +0300

    feat: error handlint middleware concated to store mw

--- src/services/store.js
+++ src/services/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productsApi } from "./productsApi";
 import { filterSlice } from "./filterProducts/filterSlice";
+import { errorHandlingMiddleware } from "@/utils/errorHandlingMiddleware";
 
 export const store = configureStore({
   reducer: {
     [productsApi.reducerPath]: productsApi.reducer,
     [filterSlice.name]: filterSlice.reducer,
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(productsApi.middleware),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(productsApi.middleware).concat(errorHandlingMiddleware),
 });
