6d476b38a8676031e968bc1aa32e4bbec793ef83
KirillDanProg <kirill2898v@gmail.com>
2024-02-28 17:24:47 +0300

    feat: selector function added to get all params at once or each one separatly

---
+++ src/services/filterProducts/filterSelectors.js
@@ -0,0 +1,5 @@
+export const selectOffset = state => state.filter.params.offset;
+export const selectPrice = state => state.filter.params.price;
+export const selectProductQuery = state => state.filter.params.product;
+export const selectBrand = state => state.filter.params.brand;
+export const selectParams = state => state.filter.params;
