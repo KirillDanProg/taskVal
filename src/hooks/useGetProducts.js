4b0d8b787d34681feb490517ff722339481bae96
KirillDanProg <kirill2898v@gmail.com>
2024-02-28 17:29:42 +0300

    feat: useSelector added to get params, useEffect reacts on params change and trigger new request

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,19 +1,28 @@
 import { useEffect } from "react";
 import { useGetEntitiesByActionMutation } from "@/services/productsApi";
+import { useSelector } from "react-redux";
+import { selectParams } from "@/services/filterProducts/filterSelectors";
 
-export const useGetProducts = offset => {
+export const useGetProducts = () => {
+  const { limit, offset, ...filterParams } = useSelector(selectParams);
   const [getEntities, { data, error, isLoading, originalArgs }] = useGetEntitiesByActionMutation();
   const isProducts = originalArgs?.action === "get_items";
 
   useEffect(() => {
     const getProductsByIdsHandler = async () => {
+      let params = { offset, limit };
+      let action = "get_ids";
+
+      const paramsEntries = Object.entries(filterParams).filter(([key]) => filterParams[key]);
+      if (paramsEntries.length) {
+        params = Object.fromEntries(paramsEntries);
+        action = "filter";
+      }
       const { data: productIds } = await getEntities({
-        action: "get_ids",
-        params: {
-          offset,
-          limit: 50,
-        },
+        action,
+        params,
       });
+
       if (productIds) {
         await getEntities({
           action: "get_items",
@@ -24,7 +33,7 @@ export const useGetProducts = offset => {
       }
     };
     getProductsByIdsHandler();
-  }, [offset, error]);
+  }, [error, ...Object.values(filterParams)]);
 
   return { data: isProducts ? data : undefined, isLoading, error };
 };
