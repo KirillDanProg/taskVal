8f456095ad04098ff7b4da8d1540fa2fcaec27f8
KirillDanProg <kirill2898v@gmail.com>
2024-02-29 19:33:06 +0300

    fix: product name added to product card

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,11 +1,15 @@
 import s from "./Products.module.scss";
 export function Product({ productItem }) {
-  const { id, price, brand } = productItem;
+  const { id, price, brand, product } = productItem;
   return (
     <div className={s.card} key={id}>
       <div className={s.cardInfo}>
         <p className={s.textTitle}>ID: {id}</p>
         <p className={s.textBody}>Брэнд: {brand ?? "Отсутствует"}</p>
+        <p className={s.textTitle}>
+          Название товара: <br />
+          {product}
+        </p>
       </div>
       <div className={s.cardFooter}>
         <span className={s.textTitle}>Цена: {price}</span>
