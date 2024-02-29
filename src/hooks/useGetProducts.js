c5adf87fe22facc147c592eb52dd83041172b57d
KirillDanProg <kirill2898v@gmail.com>
2024-02-29 19:31:31 +0300

    refactor: comments added, dependencies variable created

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
-import { useGetEntitiesByActionMutation } from "@/services/productsApi";
 import { useSelector } from "react-redux";
+import { useGetEntitiesByActionMutation } from "@/services/productsApi";
 import { selectParams } from "@/services/filterProducts/filterSelectors";
 
 export const useGetProducts = () => {
-  const { limit, offset, ...filterParams } = useSelector(selectParams);
   const [getEntities, { data, error, isLoading, originalArgs }] = useGetEntitiesByActionMutation();
-  const isProducts = originalArgs?.action === "get_items";
+  const { limit, offset, ...filterParams } = useSelector(selectParams);
+  const dependencies = Object.values(filterParams); //deps to trigger effect if url params changed
+  const isProducts = originalArgs?.action === "get_items"; //isProducts flag need to return data only when items received
 
   useEffect(() => {
-    const getProductsByIdsHandler = async () => {
+    (async () => {
       let params = { offset, limit };
       let action = "get_ids";
 
@@ -17,7 +18,8 @@ export const useGetProducts = () => {
       if (paramsEntries.length) {
         params = Object.fromEntries(paramsEntries);
         action = "filter";
-      }
+      } // reasign action and params if there is an url param
+
       const { data: productIds } = await getEntities({
         action,
         params,
@@ -31,9 +33,8 @@ export const useGetProducts = () => {
           },
         });
       }
-    };
-    getProductsByIdsHandler();
-  }, [error, offset, ...Object.values(filterParams)]);
+    })();
+  }, [error, offset, ...dependencies]);
 
   return { data: isProducts ? data : undefined, isLoading, error };
 };
