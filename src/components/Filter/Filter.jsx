cd1adf909803476fe12114f801fc541fa7166834
KirillDanProg <kirill2898v@gmail.com>
2024-02-29 19:38:44 +0300

    feat: filter component wrapped with memo, form tag removed

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,19 +2,16 @@ import s from "./Filter.module.scss";
 import { FilterInput } from "./FilterSearchInput/FilterInput";
 import { FilterSelect } from "./FilterBrandSelect/FilterSelect";
 import { PriceInput } from "./FilterPriceInput/PriceInput";
+import React from "react";
 
-export function Filter() {
-  const onSubmitHandler = e => {
-    e.preventDefault();
-  };
-
+export const Filter = React.memo(() => {
   return (
     <div className={s.filterContainer}>
-      <form onSubmit={onSubmitHandler} className={s.form}>
-        <FilterSelect />
-        <FilterInput />
-        <PriceInput />
-      </form>
+      <FilterSelect />
+      <FilterInput />
+      <PriceInput />
     </div>
   );
-}
+});
+
+Filter.displayName = "Filter";
