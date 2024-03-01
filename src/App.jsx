a656c2de7b9102baf2d90885ce47a7bf36ca2e6d
KirillDanProg <kirill2898v@gmail.com>
2024-03-01 16:58:21 +0300

    refactor: useGetProducts and useSetFilters moved to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Products } from "@/components/Products/Products";
 import { Pagination } from "@/components/Pagination/Pagination";
 import { Filter } from "@/components/Filter/Filter";
+import { useGetProducts } from "@/hooks/useGetProducts";
+import { useSetFilteres } from "@/hooks/useSetFilteres";
 
 export function App() {
+  useSetFilteres();
+
+  const { data, isLoading } = useGetProducts();
+
   return (
     <div className="app">
+      <h1>TaskTestValantis</h1>
       <Filter />
-      <Products />
-      <Pagination />
+      <Products products={data} isLoading={isLoading} />
+      <Pagination isMore={data?.length > 40} isLoading={isLoading} />
     </div>
   );
 }
